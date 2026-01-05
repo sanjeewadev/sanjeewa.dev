55b2fe491204e39a6e74a4b333f50849c0e7ee42
sanjeewadev <svk6061@gmail.com>
2026-01-05 13:35:56 +0530

    create test enviarenment for test api

--- src/js/projectsCard.js
+++ src/js/projectsCard.js
@@ -1,13 +1,22 @@
 console.log("Projects Card JavaScript loaded");
 
-fetch("./data/projects/cards/projectcard1.json")
-  .then((response) => response.json())
-  .then((data) => {
-    console.log("Project Card Data:", data);
-    const projectCard = document.createElement("div");
-    projectCard.className = "project-card";
+const projectcardfiles = [
+  "./data/projects/cards/projectcard1.json",
+  "./data/projects/cards/projectcard2.json",
+  "./data/projects/cards/project1.json",
+];
 
-    projectCard.innerHTML = `
+const container = document.getElementById("project-card-container");
+
+for (let i = 0; i < projectcardfiles.length; i++) {
+  fetch(projectcardfiles[i])
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Project Card Data:", data);
+      const projectCard = document.createElement("div");
+      projectCard.className = "projects-card";
+
+      projectCard.innerHTML = `
       <img
         class="projects-coming-soon-img"
         src="${data.image}"
@@ -17,8 +26,7 @@ fetch("./data/projects/cards/projectcard1.json")
       <p>${data.description}</p>
       <a href="/project-details" class="button">View Details</a>
   `;
-
-    const container = document.getElementById("project-card-container");
-    console.log(container);
-    container.appendChild(projectCard);
-  });
+      console.log(container);
+      container.appendChild(projectCard);
+    });
+}
