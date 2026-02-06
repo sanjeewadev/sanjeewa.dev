9885efc2ae0a8f49591b6ae4699da071c30d8ecb
sanjeewa-dev <svk6061@gmail.com>
2026-02-06 21:49:10 +0530

    finish my sanjeewa.dev project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // This fixes the "Invalid URL" error
+  site: "https://sanjeewa.dev",
+
+  integrations: [sitemap()]
+});
--- src/js/Loading.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const loader = document.querySelector(".loading-page-section");
-
-  setTimeout(() => {
-    if (loader) {
-      loader.classList.add("hidden");
-    }
-  }, 0); //5500
-});
